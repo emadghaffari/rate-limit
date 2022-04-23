bcefc317463e6af76e841abef143e822b5374dbd
Emad Ghaffari <emadghaffariii@gmail.com>
2022-04-23 18:59:20 +0430

    add comment

--- bucket.go
+++ bucket.go
@@ -13,12 +13,14 @@ type Bucket struct {
 	CurrentTokens      int64         `json:"current_tokens"`
 	LastTokenTimestamp time.Time     `json:"last_token_timestamp"`
 	Duration           time.Duration `json:"duration"`
-	rds                *redis.Client
+	rds                *redis.Client `json:"-"`
 }
 
+// IsRequestAllowed: check the request with the tokens
 func (b *Bucket) IsRequestAllowed(tokens int64) bool {
 	now := time.Now()
 	end := time.Since(b.LastTokenTimestamp)
+
 	tokensTobeAdded := (end.Nanoseconds() * b.Rate) / 1000000000
 	b.CurrentTokens = int64(math.Min(float64(b.CurrentTokens+tokensTobeAdded), float64(b.MaxTokens)))
 	b.LastTokenTimestamp = now
